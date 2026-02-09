9815694d83a633f5f08d1c4dee54436d20124aa9
Chong <arnoldchongyutze@gmail.com>
2026-02-09 11:10:08 +0800

    Import Leong Wei Lee Task

---
+++ Ass-ParkingLotManagementSystem/src/main/java/controller/EntryService.java
@@ -0,0 +1,27 @@
+package main.java.controller;
+
+
+public class EntryService {
+
+    public Ticket processEntry(String plateNumber, String vehicleType, ParkingSpot selectedSpot) throws Exception {
+        
+        // 1. Validation
+        if (selectedSpot.isOccupied()) {
+            throw new Exception("Error: Spot " + selectedSpot.getSpotId() + " is occupied.");
+        }
+
+        // 2. Create Vehicle
+        Vehicle vehicle = VehicleFactory.createVehicle(vehicleType, plateNumber);
+
+        // 3. Validate Rules (Member 2's core logic)
+        if (!vehicle.canParkIn(selectedSpot.getType())) {
+            throw new Exception("Error: " + vehicleType + " cannot park in " + selectedSpot.getType());
+        }
+
+        // 4. Park
+        selectedSpot.assignVehicle(vehicle);
+
+        // 5. Ticket
+        return new Ticket(vehicle, selectedSpot);
+    }
+}

9e3ea66dfd115c2595e315b1b316d6c4c593668a
Chong <arnoldchongyutze@gmail.com>
2026-02-09 13:31:08 +0800

    Updated ParkingSpot.java

--- Ass-ParkingLotManagementSystem/src/main/java/model/ParkingSpot.java
+++ Ass-ParkingLotManagementSystem/src/main/java/model/ParkingSpot.java
@@ -5,16 +5,56 @@ public class ParkingSpot {
     private ParkingSpotType type;
     private boolean isOccupied;
     private Vehicle currentVehicle;
+    private int floorNumber;      // NEW
+    private String row;           // NEW
+    private double hourlyRate;    // NEW
+    private int spotNumber;       // NEW: For unique spot number
 
-    public ParkingSpot(String spotId, ParkingSpotType type) {
-        this.spotId = spotId;
+    public ParkingSpot(int floorNumber, String row, int spotNumber, ParkingSpotType type) {
+        this.floorNumber = floorNumber;
+        this.row = row;
+        this.spotNumber = spotNumber;
         this.type = type;
         this.isOccupied = false;
+        
+        // Generate spot ID in format "F{floor}-R{row}-S{spotNumber}"
+        this.spotId = String.format("F%d-R%s-S%d", floorNumber, row, spotNumber);
+        
+        // Set hourly rate based on spot type
+        switch (type) {
+            case COMPACT:
+                this.hourlyRate = 2.0;      // RM 2/hour
+                break;
+            case REGULAR:
+                this.hourlyRate = 5.0;      // RM 5/hour
+                break;
+            case HANDICAPPED:
+                this.hourlyRate = 2.0;      // RM 2/hour
+                break;
+            case RESERVED:
+                this.hourlyRate = 10.0;     // RM 10/hour
+                break;
+            default:
+                this.hourlyRate = 5.0;      // Default rate
+        }
     }
 
-    public boolean isOccupied() { return isOccupied; }
-    public ParkingSpotType getType() { return type; }
+    // Getters
     public String getSpotId() { return spotId; }
+    public ParkingSpotType getType() { return type; }
+    public boolean isOccupied() { return isOccupied; }
+    public int getFloorNumber() { return floorNumber; }      // NEW
+    public String getRow() { return row; }                   // NEW
+    public double getHourlyRate() { return hourlyRate; }     // NEW
+    public int getSpotNumber() { return spotNumber; }        // NEW
+    
+    // For displaying in dropdown
+    @Override
+    public String toString() {
+        String status = isOccupied ? "[OCCUPIED]" : "[FREE]";
+        return String.format("%s (%s) %s - RM%.1f/hour", 
+            spotId, type, status, hourlyRate);
+    }
 
     public void assignVehicle(Vehicle v) {
         this.currentVehicle = v;
